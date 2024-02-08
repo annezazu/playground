4a83d9929e2b675915f06d31084b9a0b67f7d3b9
annezazu <annezazu@gmail.com>
2024-02-08 11:45:48 -0700

    Trying again - YouTube video showcasing small changes for 6.5
    Also exported as a zip file.
    
    * [Preview loaded from this PR – available **before** this PR is merged](https://playground.wordpress.net/?import-site=https%3A%2F%2Fraw.githubusercontent.com%2Fannezazu%2Fplayground%2Fplayground-changes-2024-02-08T18-45-04-829Z%2Fplayground%2Fplayground.zip)
    * [Preview loaded from the main branch – available **after** this PR is merged](https://playground.wordpress.net/?import-site=https%3A%2F%2Fraw.githubusercontent.com%2Fannezazu%2Fplayground%2Fmain%2Fplayground%2Fplayground.zip)

---
+++ playground/plugins/gutenberg-71/build/block-library/blocks/image/view.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-interactivity'), 'version' => '6fb8571f96cdb79cc6e9');
---
+++ playground/plugins/gutenberg-71/build/interactivity/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'f9c37d3b56ad5db1aaf4', 'type' => 'module');
